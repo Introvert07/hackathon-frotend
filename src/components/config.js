6d13b69b327cb4de7cda19fe4308ec3e7f5a90d4
Introvert07 <maheshkushwah236@gmai.com>
2025-06-18 20:29:01 +0530

    baseurlupdated

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+//export const BASE_URL = "http://localhost:5000";
 
+export const BASE_URL = "https://hackathon-backend-mahesh-kushwahs-projects.vercel.app/";
